cf76dbc91b3e25de02d5e8e67773c859cda48ba4
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-08-04 16:53:39 +0200

    printing of rounds

--- test.py
+++ test.py
@@ -1,21 +1,24 @@
+from venv import create
 from models import *
 from views import *
 from controllers import *
 from faker import Faker
 import random
 import datetime
-import math
 
 
 if __name__ == "__main__":
 	players = [ Player(firstname=Faker().first_name(), lastname=Faker().last_name(), gender=random.choice(["M", "F"]), birthdate=Faker().date_of_birth(), elo_rank=random.randint(1400, 2400)) for p in range(16) ]
-	tournament = Tournament(name="Unreal Tournament", players=players, location="Cupertino, CA", description="A test tournament", time_control=0, date=datetime.datetime.now())
+	tournament = Tournament(name="Unreal Tournament", players=players, location="Cupertino, CA", description="A test tournament", date=datetime.datetime.now())
 
+	# FIXME: 1 ranking round before the actual tournament
 	for p in swiss(tournament.players):
 		print(f"{p[0].firstname} {p[0].lastname} (ELO: {p[0].elo_rank}) VS. {p[1].firstname} {p[1].lastname} (ELO: {p[1].elo_rank})")
 
-	print(f"Number of rounds: {tournament.nb_of_rounds}")
-	print(tournament.date)
+	rounds = [ Round(n + 1, datetime.datetime.now(), datetime.datetime.now() + datetime.timedelta(hours=1)) for n in range(tournament.nb_of_rounds) ]
+
+	for r in rounds:
+		print(f"Round {r.nb}: {vars(r)}")
 	# printTournamentInfos(tournament)
 	# printPlayerInfos(players[0])
 	# printMatchInfos(players[0])
