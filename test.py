d4c870124451a552fc350d50d0bc25462a3a7d1e
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-08-12 11:56:09 +0200

    test app simplification

--- test.py
+++ test.py
@@ -18,23 +18,17 @@ if __name__ == "__main__":
 
 	players = [ Player(id=_, firstname=Faker().first_name(), lastname=Faker().last_name(), gender=random.choice(["M", "F"]), birthdate=str(Faker().date_of_birth()), elo_rank=random.randint(1200, 1400)) for _ in range(8) ]
 	tournament = Tournament(name="Unreal Tournament", location="Cupertino, CA", description="A test tournament", date=str(datetime.datetime.now()))
-
 	rounds = [ Round(n + 1, str(datetime.datetime.now()), str(datetime.datetime.now()) + str(datetime.timedelta(hours=1))) for n in range(tournament.nb_of_rounds) ]
 	matches = [ Match(paired_players_ids=[_]) for _ in swiss(players) ]
-	print(rounds)
-	print(matches)
 
 	# Objects to dictionaries
 	players_dict = []
 	rounds_dict = []
 	matches_dict = []
 
-	for _ in players:
-		players_dict.append(_.__dict__)
-	for _ in rounds:
-		rounds_dict.append(_.__dict__)
-	for _ in matches:
-		matches_dict.append(_.__dict__)
+	for _ in players: players_dict.append(_.__dict__)
+	for _ in rounds: rounds_dict.append(_.__dict__)
+	for _ in matches: matches_dict.append(_.__dict__)
 
 	# db insert
 	tournamentTable.insert(tournament.__dict__)
