dd03b375db326085bd05ca71c1ef7867b646c6b6
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-08-28 20:24:19 +0200

    tabs to spaces

--- models.py
+++ models.py
@@ -2,48 +2,48 @@ import math
 
 
 class Player:
-    instances = []
+	instances = []
 
-    def __init__(self, player_id, firstname, lastname, birthdate, gender, elo_rank) -> None:
-        self.id = player_id
-        self.firstname = firstname
-        self.lastname = lastname
-        self.birthdate = birthdate
-        self.gender = gender
-        self.elo_rank = elo_rank
-        self.__class__.instances.append(self)
+	def __init__(self, player_id, firstname, lastname, birthdate, gender, elo_rank) -> None:
+		self.id = player_id
+		self.firstname = firstname
+		self.lastname = lastname
+		self.birthdate = birthdate
+		self.gender = gender
+		self.elo_rank = elo_rank
+		self.__class__.instances.append(self)
 
-    # FIXME: https://fr.wikipedia.org/wiki/Classement_Elo#Th%C3%A9orie_math%C3%A9matique
-    def update_rank(self, new_rank):
-        self.elo_rank = new_rank
+	# FIXME: https://fr.wikipedia.org/wiki/Classement_Elo#Th%C3%A9orie_math%C3%A9matique
+	def update_rank(self, new_rank):
+		self.elo_rank = new_rank
 
 
 class Tournament:
-    round_list = []
+	round_list = []
 
-    def __init__(self, name, location, description, date) -> None:
-        self.name = name
-        self.location = location
-        self.description = description
-        self.date = date
-        # NOTE: logarithm base 2 to figure the amount of rounds to determine a single winner.
-        self.nb_of_rounds = math.ceil(math.log2(len(Player.instances)))
+	def __init__(self, name, location, description, date) -> None:
+		self.name = name
+		self.location = location
+		self.description = description
+		self.date = date
+		# NOTE: logarithm base 2 to figure the amount of rounds to determine a single winner.
+		self.nb_of_rounds = math.ceil(math.log2(len(Player.instances)))
 
-    def add_round_to_list(self, chess_round):
-        self.__class__.round_list.append(chess_round)
+	def add_round_to_list(self, chess_round):
+		self.__class__.round_list.append(chess_round)
 
 
 class Round:
-    def __init__(self, nb, start_date, end_date) -> None:
-        self.nb = nb
-        self.start_date = start_date
-        self.end_date = end_date
+	def __init__(self, nb, start_date, end_date) -> None:
+		self.nb = nb
+		self.start_date = start_date
+		self.end_date = end_date
 
 
 class Match:
-    def __init__(self, paired_players_ids) -> None:
-        self.paired_players_ids = paired_players_ids
-        self.winner = None
+	def __init__(self, paired_players_ids) -> None:
+		self.paired_players_ids = paired_players_ids
+		self.winner = None
 
-    def end(self, winner=None):
-        self.winner = winner
+	def end(self, winner=None):
+		self.winner = winner
--- views.py
+++ views.py
@@ -6,26 +6,26 @@ from models import *
 
 # Players
 def print_player_infos(player):
-    print(vars(player))
+	print(vars(player))
 
 
 def print_player_list():
-    player_list = []
-    for p in Player.instances:
-        player_list.append(vars(p))
-    print(player_list)
+	player_list = []
+	for p in Player.instances:
+		player_list.append(vars(p))
+	print(player_list)
 
 
 # Tournaments
 def print_tournament_infos(tournament):
-    print(vars(tournament))
+	print(vars(tournament))
 
 
 # Matches
 def print_match_infos(match):
-    print(vars(match))
+	print(vars(match))
 
 
 # Rounds
 def print_rounds_infos(chess_round):
-    print(vars(chess_round))
+	print(vars(chess_round))
