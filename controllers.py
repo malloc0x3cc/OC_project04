84cb8cff08a13097d8e3fa0af631946968eb1856
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-08-28 22:12:41 +0200

    Tab to space was a mistake xD

--- controllers.py
+++ controllers.py
@@ -3,36 +3,36 @@ from models import Round
 
 
 def swiss(player_list):
-	# Bubble sort
-	swap = True
-	length = len(player_list) - 1
-	while swap:
-		i = 0
-		swap = False
-		while i < length:
-			if player_list[i].elo_rank > player_list[i + 1].elo_rank:
-				buffer = player_list[i]
-				player_list[i] = player_list[i + 1]
-				player_list[i + 1] = buffer
-				swap = True
-			i += 1
-		length -= 1
+    # Bubble sort
+    swap = True
+    length = len(player_list) - 1
+    while swap:
+        i = 0
+        swap = False
+        while i < length:
+            if player_list[i].elo_rank > player_list[i + 1].elo_rank:
+                buffer = player_list[i]
+                player_list[i] = player_list[i + 1]
+                player_list[i + 1] = buffer
+                swap = True
+            i += 1
+        length -= 1
 
-	# Pairing
-	i = 0
-	pairs = []
-	while i < (len(player_list) - 1):
-		pairs.append([player_list[i].id, player_list[i + 1].id])
-		i += 2
+    # Pairing
+    i = 0
+    pairs = []
+    while i < (len(player_list) - 1):
+        pairs.append([player_list[i].id, player_list[i + 1].id])
+        i += 2
 
-	return pairs
+    return pairs
 
 
 # Rounds
 def create_round(nb):
-	nb = f"Round {nb + 1}"
-	start_date = datetime.now()
-	end_date = ""
+    nb = f"Round {nb + 1}"
+    start_date = datetime.now()
+    end_date = ""
 
-	r = Round(nb, start_date, end_date)
-	return r
+    r = Round(nb, start_date, end_date)
+    return r
--- main.py
+++ main.py
@@ -1,65 +1,69 @@
+import datetime
+import random
 from tinydb import TinyDB
+from faker import Faker
 from controllers import swiss
 from views import *
-import datetime
-# Tests
-from faker import Faker
-import random
 
 
 if __name__ == "__main__":
-	# TODO: Check if DB already exists
-	db = TinyDB("db.json", sort_keys=True, indent=4)
-	tournamentTable = db.table("Tournament")
-	playersTable = db.table("Players")
-	roundsTable = db.table("Rounds")
-	matchesTable = db.table("Matches")
+    # TODO: Check if DB already exists
+    db = TinyDB("db.json", sort_keys=True, indent=4)
+    tournamentTable = db.table("Tournament")
+    playersTable = db.table("Players")
+    roundsTable = db.table("Rounds")
+    matchesTable = db.table("Matches")
 
-	rounds = matches = []
+    rounds = matches = []
 
-	# Menu
-	print("-- Players --")
-	players = [
-		Player(
-			player_id=_,
-			firstname=Faker().first_name(),
-			lastname=Faker().last_name(),
-			gender=random.choice(["M", "F"]),
-			birthdate=str(Faker().date_of_birth()),
-			elo_rank=random.randint(1200, 1400)) for _ in range(8)
-	]
-	print_player_list()
+    # Menu
+    print("-- Players --")
+    players = [
+        Player(
+            player_id=_,
+            firstname=Faker().first_name(),
+            lastname=Faker().last_name(),
+            gender=random.choice(["M", "F"]),
+            birthdate=str(Faker().date_of_birth()),
+            elo_rank=random.randint(1200, 1400)) for _ in range(8)
+    ]
+    print_player_list()
 
-	print("-- Tournament --")
-	tournament = Tournament(
-		name=input("Name: "),
-		location=input("Location: "),
-		description=input("Description: "),
-		date=str(datetime.datetime.now())
-	)
-	print_tournament_infos(tournament)
+    print("-- Tournament --")
+    tournament = Tournament(
+        name=input("Name: "),
+        location=input("Location: "),
+        description=input("Description: "),
+        date=str(datetime.datetime.now())
+    )
+    print_tournament_infos(tournament)
 
-	print("-- Rounds --")
-	for n in range(tournament.nb_of_rounds):
-		rounds.append(
-			Round(n + 1, str(datetime.datetime.now()), str(datetime.datetime.now()) + str(datetime.timedelta(hours=1))))
-		print("-- Matches --")
-		matches = [Match(paired_players_ids=[_]) for _ in swiss(players)]
+    print("-- Rounds --")
+    for n in range(tournament.nb_of_rounds):
+        rounds.append(
+            Round(
+                n + 1,
+                str(datetime.datetime.now()),
+                str(datetime.datetime.now()) + str(datetime.timedelta(hours=1))
+            )
+        )
+        print("-- Matches --")
+        matches = [Match(paired_players_ids=[_]) for _ in swiss(players)]
 
-	# Serialization
-	players_dict = []
-	rounds_dict = []
-	matches_dict = []
+    # Serialization
+    players_dict = []
+    rounds_dict = []
+    matches_dict = []
 
-	for _ in players:
-		players_dict.append(_.__dict__)
-	for _ in rounds:
-		rounds_dict.append(_.__dict__)
-	for _ in matches:
-		matches_dict.append(_.__dict__)
+    for _ in players:
+        players_dict.append(_.__dict__)
+    for _ in rounds:
+        rounds_dict.append(_.__dict__)
+    for _ in matches:
+        matches_dict.append(_.__dict__)
 
-	# db insert
-	tournamentTable.insert(tournament.__dict__)
-	playersTable.insert_multiple(players_dict)
-	roundsTable.insert_multiple(rounds_dict)
-	matchesTable.insert_multiple(matches_dict)
+    # db insert
+    tournamentTable.insert(tournament.__dict__)
+    playersTable.insert_multiple(players_dict)
+    roundsTable.insert_multiple(rounds_dict)
+    matchesTable.insert_multiple(matches_dict)
