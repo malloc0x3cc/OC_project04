11ff02ec69c347f4ee5e87717a13d4afc6f14f64
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2022-08-30 04:59:00 +0200

    tournament logic complete

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from views import *
 
 
 if __name__ == "__main__":
+    database_file = "db.json"
+    open(database_file, 'w').close()
     # TODO: Check if DB already exists
-    db = TinyDB("db.json", sort_keys=True, indent=4)
+    db = TinyDB(database_file, sort_keys=True, indent=4)
+    db.clear_cache()
     tournamentTable = db.table("Tournament")
     playersTable = db.table("Players")
     roundsTable = db.table("Rounds")
@@ -25,18 +28,22 @@ if __name__ == "__main__":
             lastname=Faker().last_name(),
             # gender=random.choice(["M", "F"]),
             birthdate=str(Faker().date_of_birth()),
-            elo_rank=random.randint(1200, 1400)) for _ in range(8)
+            elo_rank=random.randint(1200, 1400)
+        ) for _ in range(2)
     ]
-    print_player_list()
+    players_dict = []
+    for _ in players:
+        players_dict.append(_.__dict__)
+    playersTable.insert_multiple(players_dict)
 
     print("-- Tournament --")
     tournament = Tournament(
         name=input("Name: "),
         location=input("Location: "),
-        description=input("Description: "),
+        description=input("Description (optional): "),
         date=str(datetime.datetime.now())
     )
-    print_tournament_infos(tournament)
+    tournamentTable.insert(tournament.__dict__)
 
     print("-- Rounds --")
     for n in range(tournament.nb_of_rounds):
@@ -47,23 +54,25 @@ if __name__ == "__main__":
                 str(datetime.datetime.now()) + str(datetime.timedelta(hours=1))
             )
         )
+        print(f"Round {rounds[-1].nb}")
         print("-- Matches --")
-        matches = [Match(paired_players_ids=[_]) for _ in swiss(players)]
+        matches = [
+            Match(paired_players_ids=_) for _ in swiss(players)
+        ]
+        matches_dict = []
 
-    # Serialization
-    players_dict = []
+        print(matches)
+
+        for _ in matches:
+            print(_.paired_players_ids)
+            _.end(int(input("winner: ")))
+            for p in players:
+                if _.winner == p.id:
+                    players.remove(p)
+            matches_dict.append(_.__dict__)
+            matchesTable.insert_multiple(matches_dict)
+
     rounds_dict = []
-    matches_dict = []
-
-    for _ in players:
-        players_dict.append(_.__dict__)
     for _ in rounds:
         rounds_dict.append(_.__dict__)
-    for _ in matches:
-        matches_dict.append(_.__dict__)
-
-    # db insert
-    tournamentTable.insert(tournament.__dict__)
-    playersTable.insert_multiple(players_dict)
     roundsTable.insert_multiple(rounds_dict)
-    matchesTable.insert_multiple(matches_dict)
